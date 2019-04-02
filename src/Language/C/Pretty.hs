2c073ab4b27b0d9193a2a9fdf64d79e0763cf66b
Shuhao <rzsh2003@gmail.com>
2019-04-02 12:39:38 +0100

    fixed issue of not able to express expr like "a = {1,3,4};"

--- src/Language/C/Pretty.hs
+++ src/Language/C/Pretty.hs
@@ -435,7 +435,7 @@ instance Pretty CExpr where
     prettyPrec _p (CVar ident _) = identP ident
     prettyPrec _p (CConst constant) = pretty constant
     prettyPrec _p (CCompoundLit decl initl _) =
-        parens (pretty decl) <+> (braces . hsep . punctuate comma) (map p initl) where
+        (let declDoc = pretty decl in if isEmpty declDoc then empty else parens declDoc) <+> (braces . hsep . punctuate comma) (map p initl) where
         p ([], initializer)           = pretty initializer
         p (mems, initializer) = hcat (punctuate (text ".") (map pretty mems)) <+> text "=" <+> pretty initializer
 
